404afcaf80291de8fa6e26b880686be8c9efa7f0
Daniel Beßler <danielb@informatik.uni-bremen.de>
2015-01-15 15:38:29 +0100

    Debug rosbridge connection events.

--- knowrob.js
+++ knowrob.js
@@ -42,6 +42,16 @@ function Knowrob(options){
     this.init = function () {
       // Connect to ROS.
       ros = new ROSLIB.Ros({url : rosURL});
+      ros.on('connection', function() {
+        console.log('Connected to websocket server.');
+      });
+      ros.on('error', function(error) {
+        console.log('Error connecting to websocket server: ', error);
+      });
+      ros.on('close', function() {
+        console.log('Connection to websocket server closed.');
+      });
+
       // Create the main viewer.
       rosViewer = new ROS3D.Viewer({
         divID : canvasDiv,
